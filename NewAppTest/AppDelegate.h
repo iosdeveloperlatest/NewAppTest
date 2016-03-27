c440ab076b83902b0f1d74b4668b5045c7a6c8b4
fis <fis@fiss-mac-3.local>
2016-03-27 04:17:17 -0700

    Initial Commit

---
+++ NewAppTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewAppTest
+//
+//  Created by fis on 27/03/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewAppTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewAppTest
+//
+//  Created by fis on 27/03/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
